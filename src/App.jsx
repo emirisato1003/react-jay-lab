3ed07bcad863818bdfccb348c1898e2598592ee9
Emiri Sato <emirisato.1003@gmail.com>
2025-03-20 10:04:29 -0700

    adding list

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
-// import reactLogo from './assets/react.svg'
+import reactLogo from './assets/react.svg'
 import Name from './components/Name'
 import Position from './components/Position'
 import {Age} from './components/Age'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [testList, setTestList] = React.useState([1,2,3])
 
   return (
-      // <div className='coming-soon'>
-      //   <h1>CTD Swag</h1>
-      //   <div style={{height: 100, width: 100}}>
-      //     <img src={reactLogo} alt="react logo" />
-      //   </div>
-      //   <h2>Coming Soon...</h2>
-      // </div>
-      <div>
-        <Name />
-        <Position />
-        <Age title="age"/>
+      <div className='coming-soon'>
+        <h1>CTD Swag</h1>
+        <div style={{height: 100, width: 100}}>
+          <img src={reactLogo} alt="react logo" />
+        </div>
+        <h2>Coming Soon...</h2>
+        <ul>
+          {testList.map(item => <li key={item}>{item}</li>)}
+        </ul>
       </div>
+      // <div>
+      //   <Name />
+      //   <Position />
+      //   <Age title="age"/>
+      // </div>
   )
 
   // return React.createElement(
